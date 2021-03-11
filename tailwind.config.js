be4b34fa4569af653cf6fed94787eac9ccfcff69
ibirossi <ibi.homaidan@gmail.com>
2021-03-11 11:56:14 +0100

    fix issue with overriding tailwind font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    // fontFamily: {
+    //   body: ['Varela', 'sans-serif'],
+    //   display: ['Varela', 'sans-serif']
+    // },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }

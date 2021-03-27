f95c3da5d5753531d9b5ed1f636e89a7672ae57a
ibirossi <ibi.homaidan@gmail.com>
2021-03-27 09:51:24 +0100

    fixed styling/image positioning on blog page

--- src/components/Post.js
+++ src/components/Post.js
@@ -23,18 +23,18 @@ const Post = () => {
   }, [])
 
     return (
-        <main className="bg-green min-h-screen p-12">
+        <main className="bg-green-100 min-h-screen p-12 w-screen">
             <section className="container mx-auto">
                 <h1 className="text-5xl flex justify-center cursive">Blog Posts</h1>
-                <h2 className="text-lg text-gray-600 justify-center mb-12">Welcome to my Blog</h2>
+                <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my Blog</h2>
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
                   {postData && postData.map((post, index) => (
                     <article>
                       <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                        <span className="block h-64 rounded shadow-sm leading snug bg-white border-l-8 border-green-400" key={index}>
-                          <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className="w-full h-full object-cover absolute"/>
+                        <span className="block h-64 relative rounded shadow-sm leading snug bg-white border-l-8 border-green-400" key={index}>
+                          <img src={post.mainImage.asset.url} alt={post.mainImage.alt} className="w-full h-full rounded-r object-cover absolute"/>
                           <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
-                              <h3 className="text-gray-800 text-lg font-blog px-3 py-4 bg-red-700 bg-opacity-75 rounded">{post.title}</h3>
+                              <h3 className="text-gray-800 text-lg font-bold px-3 py-4 bg-red-700 text-red-100 bg-opacity-75 rounded-sm">{post.title}</h3>
                           </span>
 
                         </span>
